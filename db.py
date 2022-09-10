0bb50b750909b824de6251995aaafb142b26c1b8
MNandor <mnandor@users.noreply.github.com>
2022-09-10 17:00:27 +0300

    Add listShows

--- db.py
+++ db.py
@@ -128,3 +128,25 @@ def getShowByName(name):
 		error('No show found!')
 
 	return sel
+
+
+
+def _dShow(item):
+	id, name, url, state, next, padding, _, _ = item
+	state = {0:'watching', 1:'ep began', 2:'finished', 3:'to watch', 4:'maybetry'}[state]
+	#if len(name) > 30: name = name[:28]+'..'
+	if len(url) > 15: url = url[:13]+'..'
+
+	#name = name.ljust(30, ' ')
+	url = url.ljust(15, ' ')
+
+	id = str(id).rjust(3, ' ')
+
+	print(f'{id}: [{state}] /{url}/{padding}/ N:{next} {name}')
+
+def listShows():
+	cur.execute('select * from shows where state = 0 or state = 1')
+
+	res = cur.fetchall()
+	for item in res:
+		_dShow(item)
--- r.py
+++ r.py
@@ -1,6 +1,9 @@
 #!/bin/python3
 
-from db import log, addShow, getShowByName
+from db import log, addShow, getShowByName, listShows
+import sys
+
+args = sys.argv[1:]
 
 def error(msg):
 	print(msg)
@@ -8,3 +11,11 @@ def error(msg):
 	exit()
 
 
+if len(args) == 1 and args[0] == 'list':
+	listShows()
+	exit()
+
+
+if len(args) == 1 and args[0] == 'add':
+	addShow()
+	exit()
